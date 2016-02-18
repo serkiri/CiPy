b1e9033998ee531d57296d54c7d00b9c6ddece59
Sergiy Kyrylenko, Iuliia Bilozor <sergiy.kyrylenko@travisperkins.co.uk>
2016-02-18 18:57:36 +0200

    Add MDM config

---
+++ config_mdm.py
@@ -0,0 +1,22 @@
+cipyPort = 8003
+
+jenkins_url = 'http://jenkins.pipe.soa.test01.aws.travisperkins.com:8080/'
+
+jobs = [
+    {
+        'cipyPrettyName' : 'MDM On Push',
+        'url' : jenkins_url + 'job/Branch-Entity-Service_OnPush_Master/',
+    },
+    {
+        'cipyPrettyName' : 'MDM Deploy SIT',
+        'url' : jenkins_url + 'job/Branch-Entity-Service_Deploy_ECS_SIT/',
+    },
+    {
+        'cipyPrettyName' : 'MDM SIT',
+        'url' : jenkins_url + 'job/MDM_SIT/',
+    },
+    {
+        'cipyPrettyName' : 'MDM Deploy UAT',
+        'url' : jenkins_url + 'job/Branch-Entity-Service_Deploy_ECS_UAT/',
+    },
+]
--- runcipyserver.py
+++ runcipyserver.py
@@ -127,7 +127,7 @@ if __name__ == '__main__':
     print ('starting cipy server')
     print (sys.version)
     
-    cipyVersion = "2.14"
+    cipyVersion = "2.15"
 
     jenkins_updater()
 
